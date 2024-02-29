3955d91379cb69d98f2995774eac204db1f08d79
unknown <adam.zen@hotmail.co.uk>
2024-02-29 10:12:12 +0000

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    private int[][] board;
+    Scanner scanner = new Scanner(System.in);
+    Random random = new Random();
+
+
+    public void start() {
+        System.out.println("Welcome to Minesweeper");
+        int minesPlaced = 0;
+
+
+    }
+
+    public static void main(String[] args) {
+        Main MineSweeper = new Main();
+        MineSweeper.start();
+
+        // create board - depending on difficulty how
+    }
+}
